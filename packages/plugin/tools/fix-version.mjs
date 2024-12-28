dede4d296c2999c9e193a0ddc9237e7d94d3387f
GregRos <life.gregr@gmail.com>
2024-12-28 22:21:08 +0200

    fixed various scripts and compilation stuff

--- packages/plugin/tools/fix-version.mjs
+++ packages/plugin/tools/fix-version.mjs
@@ -1,34 +1,45 @@
 import { exec, execSync } from "child_process";
 import { readFileSync, writeFileSync } from "fs";
 import shelljs from "shelljs";
-const { echo, cat } = shelljs;
+const { echo, cat, cd } = shelljs;
 
+cd(`${import.meta.dirname}/..`)
 const targetVersion = process.argv[2] ?? process.env.npm_package_version;
-
+echo(`fixing versions to: ${targetVersion}`);
 if (targetVersion !== process.env.npm_package_version) {
     const packageJson = JSON.parse(cat("package.json"));
     echo(JSON.stringify({
         ...packageJson,
         version: targetVersion
     }, null, "\t")).to("package.json");
+    echo("package.json: changed")
+} else {
+    echo("package.json: upchanged")
 }
 // read minAppVersion from manifest.json and bump version to target version
-const manifest = JSON.parse(readFileSync("../../manifest.json", "utf8"));
+const pathManifest = "../../manifest.json";
+const manifest = JSON.parse(cat(pathManifest));
 const { minAppVersion } = manifest;
 
 if (targetVersion !== manifest.version) {
     echo(JSON.stringify({
         ...manifest,
         version: targetVersion
-    }, null, "\t")).to("manifest.json")
+    }, null, "\t")).to(pathManifest)
+    echo("manifest.json: changed")
+} else {
+    echo("manifest.json: upchanged")
 }
-const versions = JSON.parse(cat("../../versions.json"));
+const pathVersions = "../../versions.json";
+const versions = JSON.parse(cat(pathVersions));
 if (versions[targetVersion] !== minAppVersion) {
     echo(JSON.stringify({
         ...versions,
         [targetVersion]: minAppVersion
-    }, null, "\t")).to("versions.json");
+    }, null, "\t")).to(pathVersions);
+    echo("versions.json: changed")
+} else {
+    echo("versions.json: upchanged")
 }
-
-execSync("git add manifest.json versions.json package.json");
-execSync(`git commit -m "bump version to ${targetVersion}"`);
+exec("git add manifest.json versions.json");
+exec(`git commit -m "bump version to ${targetVersion}"`);
